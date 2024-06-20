9f256275e18b2e3e57b65922a86cb6c346c8a3fc
GabrielMax-1 <gjmaxym205@gmail.com>
2024-06-19 22:53:20 -0500

    Levelset fixed 19.06

--- Controllers/Networking.gd
+++ Controllers/Networking.gd
@@ -9,10 +9,10 @@ func send_level(file_path: String, name: String, creator: String):
 	nm = name
 	ct = creator
 
-	var url = "https://00firebeat99.000webhostapp.com/upload.php"  # Reemplaza con la URL correcta de tu servidor
+	var url = "https://00firebeat99.000webhostapp.com/upload.php"
 
 	# Configurar la solicitud POST
-	var headers = ["Content-Type: multipart/form-data; boundary=boundary"]  # Tipo de contenido para subir archivos
+	var headers = ["Content-Type: multipart/form-data; boundary=boundary"]
 
 	# Crear el cuerpo de la solicitud para subir el archivo
 	var form_data = create_multipart_form_data("file", file_path)
@@ -31,7 +31,7 @@ func create_multipart_form_data(field_name, file_path):
 	var file = File.new()
 	if file.open(file_path, File.READ) != OK:
 		print("Error al abrir el archivo:", file_path)
-		return form_data  # Devolver un bytearray vacío si hay un error
+		return form_data
 
 	# Leer el contenido del archivo
 	var file_content = file.get_buffer(file.get_len())
@@ -62,8 +62,10 @@ func _on_request_completed(result, response_code, headers, body):
 		var regex = RegEx.new()
 		regex.compile("El archivo ha sido subido como: (\\d+\\.\\w+)")
 		var matchs = regex.search(response_body)
-		send_level_data(matchs.get_string(1), nm, ct)
-		# Aquí puedes manejar la respuesta del servidor según tus necesidades
+		if matchs:
+			send_level_data(matchs.get_string(1), nm, ct)
+		else:
+			print("No se encontró el ID del archivo en la respuesta.")
 	else:
 		print("Error en la solicitud:")
 		print("Código de respuesta:", response_code)
@@ -72,9 +74,9 @@ func _on_request_completed(result, response_code, headers, body):
 func send_level_data(lid: String, name: String, creator: String):
 	var http_request2 = HTTPRequest.new()
 	add_child(http_request2)
-	
+
 	var url = "https://00firebeat99.000webhostapp.com/levelset.php"
-	
+
 	# Crear un diccionario con los datos a enviar
 	var request_data = {
 		"lid": lid,
@@ -83,23 +85,23 @@ func send_level_data(lid: String, name: String, creator: String):
 		"time": str(OS.get_unix_time()),
 		"version": 1
 	}
-	
+
 	# Convertir el diccionario a una cadena JSON
-	var json_request_data = JSON.print(request_data)
-	
+	var json_request_data = JSON.print(request_data).to_utf8()
+
 	# Conectar la señal de solicitud completada
 	http_request2.connect("request_completed", self, "_on_request2_completed")
-	
+
 	# Enviar la solicitud con el método POST y los datos JSON
 	var custom_headers = ["Content-Type: application/json"]
-	http_request2.request(url, custom_headers, true, HTTPClient.METHOD_POST, json_request_data)
-	
+	http_request2.request_raw(url, custom_headers, true, HTTPClient.METHOD_POST, json_request_data)
+
 	print(request_data)
+
 func _on_request2_completed(result, response_code, headers, body):
 	if result == OK:
 		var response_body = body.get_string_from_utf8()
 		print("Respuesta del servidor:", response_body)
-		# Aquí puedes manejar la respuesta del servidor según tus necesidades
 	else:
 		print("Error en la solicitud:")
 		print("Código de respuesta:", response_code)
